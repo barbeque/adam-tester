63c652ce774d6beb7ce9ede78d0078b2e46fd365
Mike <barbeque@users.noreply.github.com>
2024-08-31 11:02:51 -0600

    Fumbling text display

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 MAME = /Users/mike/mame0253-arm64/mame
 mame_dir = $(dir $(MAME))
 local_path = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-mame_args = -skip_gameinfo -window # -aviwrite pc6001.avi
+mame_args = -skip_gameinfo -window # -aviwrite adam.avi
 asm_args = -w
 asm = zasm
 
@@ -11,7 +11,7 @@ badam.bin: badam.asm
 all: badam.bin 
 
 clean:
-	rm -f pc6001.bin pc6001.lst meules.asm
+	rm -f badam.bin badam.lst
 
 run: badam.bin
 	cd $(mame_dir) && $(MAME) adam $(mame_args) -cart1 $(local_path)badam.bin
--- badam.asm
+++ badam.asm
@@ -1,8 +1,8 @@
 .org $8000
 
 ; standard colecovision header
-HEADER_GAME: .db $aa, $55
-; HEADER_TEST: .db $55 $aa
+;HEADER_GAME: .db $aa, $55
+HEADER_TEST: .db $55, $aa
 
 SPRITE_TABLE: .dw $0000
 SPRITE_ORDER_TABLE: .dw $0000
@@ -16,10 +16,55 @@ NMI_INT_VECT: .dw $0000
 .org $8024
 GAME_NAME: .ascii "LEADEDSOLDER.COM/ADAM TESTER!/2024"
 
+#define MODE_1      $1f85
+#define FILL_VRAM   $1f82 ; HL = address, DE = count, A = value
+#define INIT_TABLEP $1f8b
+#define LOAD_ASCII  $1f7f
+#define PUT_VRAM    $1fbe ; A = table code, DE = start index, HL = data, iy = count
+#define WRITE_REGISTER $1fd9 ; B = register, C = value
+
+; stolen from os7lib - https://github.com/tschak909/os7lib/blob/c2c87aa4f77016f3b2f8383d735089e64423cd1f/src/os7.h#L429
+#define MODE1_SPRITE_GENERATOR_TABLE $3800
+#define MODE1_PATTERN_COLOR_TABLE $2000
+#define MODE1_SPRITE_ATTRIBUTE_TABLE $1B00
+#define MODE1_PATTERN_NAME_TABLE $1800
+#define MODE1_PATTERN_GENERATOR_TABLE $0000
+
+; coleco enums for table codes in PUT_VRAM
+#define VDP_SPRITE_ATTRIBUTE $0
+#define VDP_SPRITE_GENERATOR $1
+#define VDP_PATTERN_NAMETABLE $2
+#define VDP_PATTERN_GENERATOR $3
+#define VDP_PATTERN_COLOUR $4
+
 entry:
-    jp entry
+    call MODE_1 ; "text mode"
+
+    ld hl, $0000
+    ld de, $4000
+    ld a, $00
+    call FILL_VRAM ; wipe out video ram
+
+    call LOAD_ASCII ; load ascii font into vram
+
+    ; set text-mode foreground and background colours
+    ld b, $07   ; tc bc fields
+    ld c, $02   ; light green background (2) black text (0)
+    call WRITE_REGISTER
+
+    ; write text
+    ld a, VDP_PATTERN_NAMETABLE
+    ld de, 0
+    ld hl, HELLO_WORLD
+    ld iy, 11
+    call PUT_VRAM ; this is writing to VRAM, but it's not showing up
+
+loop:
+    jp loop
 
     ; TODO: Figure out how to print text
     ; TODO: Switch memory map into the various ADAM modes and do a RAM test
     ; TODO: Basic read/write
     ; TODO: Count up how much RAM we actually have in each mode
+
+HELLO_WORLD: .text "HELLO WORLD"
